586deac99c9b0d50f6994a55551d1758ecec5cb6
Himumi <erliansyaha@gmail.com>
2024-01-28 14:47:35 +0900

    Refavtor - Change if statement to ternary operator

--- sub_strings.rb
+++ sub_strings.rb
@@ -4,11 +4,7 @@ def sub_strings(string, dictionary)
   string.each do |word|
     dictionary.each do |item|
       if word.include?(item)
-        if hash.key?(item)
-            hash[item] = hash[item] + 1
-        else
-            hash[item] = 1
-        end
+        hash[item] = hash.key?(item) ? hash[item] + 1 : 1
       end
     end
   end
@@ -16,3 +12,4 @@ def sub_strings(string, dictionary)
 end
 
 puts sub_strings("Howdy partner, sit down! How's it going? go! go!", ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
+# return {"how"=>2, "howdy"=>1, "part"=>1, "partner"=>1, "it"=>2, "i"=>3, "sit"=>1, "down"=>1, "own"=>1, "go"=>3, "going"=>1}
