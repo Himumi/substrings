aa5374d49edff06fbf540342207412920610b398
Himumi <erliansyaha@gmail.com>
2024-01-28 16:12:13 +0900

    Build - Create new def uses scan and reduce method

--- sub_strings.rb
+++ sub_strings.rb
@@ -10,3 +10,16 @@ end
 
 puts sub_strings("Howdy partner, sit down! How's it going? go! go!", ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
 # return {"how"=>2, "howdy"=>1, "part"=>1, "partner"=>1, "it"=>2, "i"=>3, "sit"=>1, "down"=>1, "own"=>1, "go"=>3, "going"=>1}
+
+# Use scan and reduce method
+
+def sub_strings_scan(string, dictionary)
+  dictionary.reduce(Hash.new(0)) do |hash, item|
+    count = string.downcase.scan(item).size
+    hash[item] = count if count > 0
+    hash
+  end
+end
+
+puts sub_strings_scan("Howdy partner, sit down! How's it going? go! go!", ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
+# return {"down"=>1, "go"=>3, "going"=>1, "how"=>2, "howdy"=>1, "it"=>2, "i"=>3, "own"=>1, "part"=>1, "partner"=>1, "sit"=>1}
